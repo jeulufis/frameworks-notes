e8343723ce7ea00aaf0e9321e108c2e26ff055af
Juan Gerardo Eulufi Salazar <juan.eulufi.sa@gmail.com>
2024-06-06 21:26:45 -0400

    Create #01.js

---
+++ javascript/#01.js
@@ -0,0 +1,26 @@
+// #00 SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO
+
+// * Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+// https://retosdeprogramacion.com/roadmap
+
+// * Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+/* https://retosdeprogramacion.com/roadmap */
+
+// * Crea una variable (y una constante si el lenguaje lo soporta).
+let variable = "Esta es una variable";
+const variable_const = "Variable constante";
+
+// * Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).
+let var_string = "Hola";
+let var_number = 11;
+let var_bolean = true;
+let var_null = null; // No tiene ningun valor
+
+// * Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+console.log("¡Hola, JavaScript!")
+
+
+
+
+
+
